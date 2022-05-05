beb4596ecf54f8f6d91c537619f1d3bcfe2c5ac4
vikki-viks <vicka.kudryavtseva@yandex.ru>
2022-05-05 11:40:29 +0300

    deleted app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './products/dto/users/users.entity';
@@ -19,7 +17,7 @@ import { User } from './products/dto/users/users.entity';
       synchronize: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
